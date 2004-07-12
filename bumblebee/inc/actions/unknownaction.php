a0ff124dd60edad6527ba621b46daf8bee14f26c
stuart <>
2004-07-12 11:37:04 +0000

    *** empty log message ***

--- bumblebee/inc/actions/unknownaction.php
+++ bumblebee/inc/actions/unknownaction.php
@@ -10,7 +10,7 @@ function actionUnknown($action)
   echo "<h2>Error</h2>"
   ."<p>An unknown error occurred. I was asked to perform the "
   ."action '$action' ($verb,$code) but I don't know how to do that.</p>"
-  ."<p>Please contact <a href='mailto:$ADMINEMAIL'>the system"
+  ."<p>Please contact <a href='mailto:$ADMINEMAIL'>the system "
   ."administrator</a> to report this error.</p>";
 }
 
