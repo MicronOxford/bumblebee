ba695f4e21d6d48ff2bb69a3410bb54ff03e8a5e
stuart <>
2005-05-02 12:22:37 +0000

    remove dead code from previous changes

--- bumblebee/inc/bb/costs.php
+++ bumblebee/inc/bb/costs.php
@@ -19,34 +19,18 @@ class ClassCost extends DBRow {
     $f->required = 1;
     $f->isValidTest = 'is_nonempty_string';
     $this->addElement($f);
-    #$f = new RadioList("class", "Charging class");
-    //$f->connectDB("instrumentclass", array("id", "name"));
-    //$classexample = new ExampleEntries("id","instruments","class","name",3);
-    //$classexample->separator = '; ';
-    //$f->setFormat("id", "%s", array("name"), " (%40.40s)", $classexample);
-    #$label = new ExampleEntries('id','instruments','class','name',3);
-    #$label->separator = '; ';
-    #$f->setFormat("id", "%s", array("name"), " (%40.40s)", $classexample);
 
     $f = new JoinData('costs',
                        'userclass', $this->id,
                        'classlabel', 'Cost settings');
     $instrfield = new DropList('instrumentclass', 'Instrument Class');
     $instrfield->connectDB('instrumentclass', array('id', 'name'));
-    //$instrfield->prepend(array('0','(none)'));
-    //$instrfield->setDefault(0);
     $classexample = new ExampleEntries('id','instruments','class','name',3);
     $classexample->separator = '; ';
     $instrfield->setFormat('id', '%s', array('name'), ' (%40.40s)', $classexample);
     $instrfield->editable=0;
     $f->addElement($instrfield);
 
-    //$label = new TextField('', 'User class');
-    //$label->editable = 0;
-    //$instrex = new ExampleEntries('id','instruments','class','name',3);
-    //$instrex->separator = '; ';
-    //$label->setFormat("id", "%40.40s", $instrex);
-    //$f->addElement($label);
     $f->addElement($instrfield);
     $cost = new TextField('costfullday', 'Full day cost');
     $f->addElement($cost);
@@ -56,26 +40,11 @@ class ClassCost extends DBRow {
     $f->addElement($hours);
     $f->joinSetup('instrumentclass', array('minspare' => 0));
     $f->colspan = 2;
-    //preDump($f);
     $this->addElement($f);
 
     $this->fill($id);
     $this->dumpheader = 'Cost object';
   }
-/*    
-          $q = "SELECT costs.id AS costid,instrumentclass.id AS instrclassid,"
-              ."instrumentclass.name AS instrclassname, "
-              ."cost_hour, cost_halfday,cost_fullday,"
-              ."userclass.name AS userclassname "
-          ."FROM instrumentclass "
-          #."LEFT JOIN instrumentclass ON instrumentclass.id=costs.instrumentclass "
-          ."LEFT JOIN costs ON instrumentclass.id=costs.instrumentclass "
-          ."LEFT JOIN userclass on userclass.id=costs.userclass "
-          #."LEFT JOIN stdrates on stdrates.instrid=costs.id "
-          #."LEFT JOIN instruments on stdrates.instrid=instruments.id "
-          ."WHERE costs.userclass='$userclass' "
-          ."ORDER BY instrumentclass.name";
-          #."ORDER BY instruments.name";*/
 
   function display() {
     return $this->displayAsTable();
