d437f09fab3cd2a0fb3533d4e55c13a29c33f238
stuart <>
2005-07-06 04:40:56 +0000

    * bump version numbers 0.9.9.2-1.0rc3

--- bumblebee/config/config.php
+++ bumblebee/config/config.php
@@ -28,6 +28,6 @@ if (!empty($CONFIG['main']['ExtraIncludePath'])) {
   set_include_path($CONFIG['main']['ExtraIncludePath'].':'.get_include_path());
 }
 
-$BUMBLEBEEVERSION = '0.9.9.1-1.0rc2';
+$BUMBLEBEEVERSION = '0.9.9.2-1.0rc3';
 
 ?> 
