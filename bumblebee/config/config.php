b4d8ae36282760eefc3137f8fd37a96594395528
stuart <>
2005-08-25 05:14:27 +0000

    * tagged as 0.9.9.4-1.0rc4

--- bumblebee/config/config.php
+++ bumblebee/config/config.php
@@ -28,6 +28,6 @@ if (!empty($CONFIG['main']['ExtraIncludePath'])) {
   set_include_path($CONFIG['main']['ExtraIncludePath'].PATH_SEPARATOR.get_include_path());
 }
 
-$BUMBLEBEEVERSION = '0.9.9.3';
+$BUMBLEBEEVERSION = '0.9.9.4-1.0r4';
 
 ?> 
