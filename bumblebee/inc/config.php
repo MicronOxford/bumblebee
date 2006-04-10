b873c280be25d3340ca403a93849dd24ab4675cd
stuart <>
2006-04-10 22:46:49 +0000

    * update locale handling

--- bumblebee/inc/config.php
+++ bumblebee/inc/config.php
@@ -87,11 +87,11 @@ ini_set("session.use_only_cookies",1); #don't permit ?PHPSESSID= stuff
 
 if ($CONFIG['error_handling']['AllWarnings']) {
   //this is nice for development but probably turn it off for production
-  ini_set("error_reporting",E_ALL); #force all warnings to be echoed
+  ini_set("error_reporting", E_ALL); #force all warnings to be echoed
   /** load all php files */
   define('LOAD_ALL_PHP_FILES', 1);
 } else {
-  ini_set("error_reporting",E_ERROR); #only errors should be echoed
+  ini_set("error_reporting", E_ERROR); #only errors should be echoed
   /** load only the php files required to fullfill this request) */
   define('LOAD_ALL_PHP_FILES', 0);
 }
