08046f21e96258cdaa0e7c2fa8fe795ddce1340b
stuart <>
2006-04-11 23:03:15 +0000

    *** empty log message ***

--- bumblebee/inc/i18n.php
+++ bumblebee/inc/i18n.php
@@ -23,55 +23,42 @@
 * @subpackage Misc
 */
 
-
-require_once 'php-gettext/gettext.inc';
+/** php-gettext base file for gettext emulation */
+include_once 'php-gettext/gettext.inc';
+/** logging routine */
 require_once 'inc/logging.php';
 
-$encoding = 'UTF-8';
-/** package name for i18n (mo name) */
-define('PACKAGE', 'bumblebee'); 
-
-#$locale = (isset($_GET['lang']))? $_GET['lang'] : DEFAULT_LOCALE;
+// this could be done per-user, but that would be more difficult...
+//$locale = (isset($_GET['lang']))? $_GET['lang'] : DEFAULT_LOCALE;
 $locale = $CONFIG['language']['locale'];
 
-// gettext setup
-T_setlocale(LC_MESSAGES, $locale);
-// Set the text domain as 'messages'
-$domain = 'messages';
-T_bindtextdomain(PACKAGE, $CONFIG['language']['translation_base']);
-T_bind_textdomain_codeset(PACKAGE, $encoding);
-T_textdomain(PACKAGE);
-
-if (!locale_emulation()) {
-  logmsg(9, "using C gettext for locale '$locale'");
+// work out if php-gettext is installed on this system
+if (function_exists('T_setlocale') && function_exists('T_')) {
+  //it is
+  $encoding = 'UTF-8';
+  /** package name for i18n (mo name) */
+  define('PACKAGE', 'bumblebee'); 
+  
+  
+  // gettext setup
+  T_setlocale(LC_MESSAGES, $locale);
+  // Set the text domain as 'messages'
+  $domain = 'messages';
+  
+  T_bindtextdomain(PACKAGE, $CONFIG['language']['translation_base']);
+  T_bind_textdomain_codeset(PACKAGE, $encoding);
+  T_textdomain(PACKAGE);
+  
+  if (!locale_emulation()) {
+    logmsg(9, "using C gettext for locale '$locale'");
+  }
+  else {
+    logmsg(9, "emulating gettext for locale '$locale'");
+  }
+} else {
+  // php gettext not installed
+  logmsg(9, "Cannot find php-gettext so ignoring request for locale '$locale'");
+  function T_($s) { return $s; }
 }
-else {
-  logmsg(9, "emulating gettext for locale '$locale'");
-}
-
-// if (function_exists('gettext')) {
-//   /** package name for i18n (mo name) */
-//   define('PACKAGE', 'bumblebee'); 
-//   $lang = $CONFIG['language']['locale']; // define language: en_US, zh_CN, zh_TW 
-//   putenv("LANG=$lang"); 
-// //   $lang_found = setlocale(LC_TIME, $lang);
-// //   $lang_found = setlocale(LC_MESSAGES, $lang);
-//   /** @todo do we really want LC_ALL here? */
-//   $lang_found = setlocale(LC_ALL, $lang);
-//   if (empty($lang_found)) {
-//     /** @todo provide an error for the user here? */
-//     // FIXME: 
-//     echo "Locale error";
-//   } else {
-// //     echo "Preferred language is '$lang_found'";
-//   }
-//   bindtextdomain(PACKAGE, $CONFIG['language']['translation_base']); 
-//   textdomain(PACKAGE); 
-// } else {
-//   // if gettext isn't installed, then provide a fallback
-//   function gettext($string) {
-//     return $string;
-//   }
-// }
 
 ?> 
