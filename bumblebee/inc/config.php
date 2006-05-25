cdc60e84eae2579422097d42fe4cc12844558a7b
stuart <>
2006-05-25 18:14:56 +0000

    help installer become PHP5 compliant

--- bumblebee/inc/config.php
+++ bumblebee/inc/config.php
@@ -13,7 +13,7 @@
 */
 
 $REBASE_INSTALL = isset($REBASE_INSTALL) ? $REBASE_INSTALL : '';
-
+$NON_FATAL_CONFIG = isset($NON_FATAL_CONFIG) ? $NON_FATAL_CONFIG : false;
 
 /** location of config files
 * 
@@ -89,34 +89,38 @@ $VERBOSEDATA = $CONFIG['error_handling']['VerboseData'];
 ini_set("session.use_only_cookies",1); #don't permit ?PHPSESSID= stuff
 #ini_set("session.cookie_lifetime",60*60*1); #login expires after x seconds
 
-if ($CONFIG['error_handling']['AllWarnings']) {
-  //this is nice for development but probably turn it off for production
-  ini_set("error_reporting", E_ALL); #force all warnings to be echoed
-  /** load all php files */
-  define('LOAD_ALL_PHP_FILES', 1);
-} else {
-  ini_set("error_reporting", E_ERROR); #only errors should be echoed
-  /** load only the php files required to fullfill this request) */
-  define('LOAD_ALL_PHP_FILES', 0);
+if (! $NON_FATAL_CONFIG) {
+  if ($CONFIG['error_handling']['AllWarnings']) {
+    //this is nice for development but probably turn it off for production
+    ini_set("error_reporting", E_ALL); #force all warnings to be echoed
+    /** load all php files */
+    define('LOAD_ALL_PHP_FILES', 1);
+  } else {
+    ini_set("error_reporting", E_ERROR); #only errors should be echoed
+    /** load only the php files required to fullfill this request) */
+    define('LOAD_ALL_PHP_FILES', 0);
+  }
 }
 if (!empty($CONFIG['main']['ExtraIncludePath'])) {
   set_include_path($REBASE_INSTALL.$CONFIG['main']['ExtraIncludePath'].PATH_SEPARATOR.get_include_path());
 }
 
-if (isset($CONFIG['error_handling']['UseDBug']) && $CONFIG['error_handling']['UseDBug']) {
+if (isset($CONFIG['error_handling']['UseDBug']) && $CONFIG['error_handling']['UseDBug'] && ! $NON_FATAL_CONFIG) {
   // include the dBug pretty printer for error and debugging dumps
   // http://dbug.ospinto.com/
   include_once 'dBug.php';
 } 
 
-/** load the language pack */
-require_once 'i18n.php';
+if (! $NON_FATAL_CONFIG) {
+  /** load the language pack */
+  require_once 'i18n.php';
+}
 
 /**
 * $BUMBLEBEEVERSION is the installed version of the software
 * @global string $BUMBLEBEEVERSION
 */
 global $BUMBLEBEEVERSION;
-$BUMBLEBEEVERSION = '1.2.0-cvs';
+$BUMBLEBEEVERSION = '1.1.0';
 
 ?> 
