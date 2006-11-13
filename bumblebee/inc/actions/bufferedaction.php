d2db0d653b137be80220ed7003eaf0dff346cb5d
themill <>
2006-11-13 23:14:11 +0000

    * allow buffered documents to be automatically dealt with by the browser rather than always prompting to find out what should be done.

--- bumblebee/inc/actions/bufferedaction.php
+++ bumblebee/inc/actions/bufferedaction.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* Primitive class that allows output to be suppressed 
+* Primitive class that allows output to be suppressed
 *
 * @author    Stuart Prescott
 * @copyright  Copyright Stuart Prescott
@@ -22,35 +22,40 @@ require_once 'inc/statuscodes.php';
 require_once 'inc/actions/actionaction.php';
 
 /**
-* Primitive class that allows output to be suppressed 
+* Primitive class that allows output to be suppressed
 * @package    Bumblebee
 * @subpackage Actions
 */
 class BufferedAction extends ActionAction  {
-  /** 
+  /**
   * data stream to be buffered then output
   * @var string
   */
   var $bufferedStream;
-  /** 
+  /**
   * filename to suggest to the browser for saving the stream
   * @var string
   */
   var $filename;
-  /** 
+  /**
   * error message (if any) for display instead of the stream
   * @var string
   */
   var $errorMessage;
-  /** 
+  /**
   * mime type for instructing the browser what to do with the stream
   * @var string
   */
   var $mimetype = 'text/plain';
-  
   /**
-  * Initialising the class 
-  * 
+  * whether the file is to be displayed by the browser automatically or offered to be saved
+  * @var boolean
+  */
+  var $inline = true;
+
+  /**
+  * Initialising the class
+  *
   * @param  BumblebeeAuth $auth  Authorisation object
   * @param  array $pdata   extra state data from the call path
   * @return void nothing
@@ -61,7 +66,7 @@ class BufferedAction extends ActionAction  {
     $this->ob_flush_ok = 0;
   }
 
-  /** 
+  /**
   * Unbuffer the stream and allow the data to be output
   */
   function unbuffer() {
@@ -71,8 +76,8 @@ class BufferedAction extends ActionAction  {
       ob_end_flush();
     }
   }
-  
-  /** 
+
+  /**
   * Unbuffer the stream and get ready to drop the error message to the user
   * @param string $err error message to be output
   */
@@ -81,24 +86,25 @@ class BufferedAction extends ActionAction  {
     $this->unbuffer();
     return STATUS_ERR;
   }
-  
-  /** 
+
+  /**
   * Send the data back to the user now
   */
   function sendBufferedStream() {
     $this->outputTextFile($this->filename, $this->bufferedStream);
   }
-  
-  /** 
+
+  /**
   * send headers to the browser with the filename and the mimetype
   * @param string $filename the suggested filename to give to the browser
   */
   function startOutputTextFile($filename) {
-    header('Content-type: '.$this->mimetype); 
-    header("Content-Disposition: attachment; filename=$filename");                    
+    header('Content-type: '.$this->mimetype);
+    $disposition = ($this->inline ? 'inline' : 'attachment');
+    header("Content-Disposition: $disposition; filename=$filename");
   }
-  
-  /** 
+
+  /**
   * Dump a data stream to the user
   * @param string $filename the suggested filename to give to the browser
   * @param string $stream the data stream to be sent
@@ -108,7 +114,7 @@ class BufferedAction extends ActionAction  {
     echo $stream;
   }
 
-  /** 
+  /**
   * Save the datastream to a local file
   * @param string $filename the filename to save the data to on the server
   * @param string $stream the data stream to be saved
@@ -119,7 +125,7 @@ class BufferedAction extends ActionAction  {
     fclose($fp);
   }
 
-  /** 
+  /**
   * Work out an appropriate (and hopefully unique) filename for the data
   * Uses the Config option in bumblebee.ini [export]::filename
   * The following parameters are replaced: __date__ __action__ __what__
@@ -138,5 +144,5 @@ class BufferedAction extends ActionAction  {
   }
 
 } //class BufferedAction
- 
+
 ?>
